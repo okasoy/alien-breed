d20c8eeae6bcdc197fa9c96765b68bc894b15991
Kseniya <kseniya.shykina@student.tuke.sk>
2021-10-29 20:50:41 +0300

    teleport

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -2,6 +2,7 @@ package sk.tuke.kpi.oop.game;
 
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.actions.Invoke;
+import sk.tuke.kpi.gamelib.actions.When;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.framework.Player;
 import sk.tuke.kpi.gamelib.framework.actions.Loop;
@@ -29,10 +30,6 @@ public class Teleport extends AbstractActor {
 
     public void teleportPlayer(Player player) {
         if(this.itsTarget == null) return;
-        if(this.isTeleported == true){
-            if(this.intersects(player)) return;
-            else this.isTeleported = false;
-        }
         int x1, y1, x2, y2, x3, y3;
         x1 = this.getPosX() + this.getWidth() / 2;
         y1 = this.getPosY() + this.getHeight() / 2;
@@ -44,16 +41,25 @@ public class Teleport extends AbstractActor {
             player.setPosition(x2, y2);
             this.itsTarget.isTeleported = true;
         }
-        /*if(x3 == x1 && y3 == y1) {
-            player.setPosition(x2, y2);
-            this.itsTarget.isTeleported = true;
-        }*/
+    }
+
+    public boolean wasTeleported(Player player){
+        if(this.isTeleported == true){
+            if(this.intersects(player)) return false;
+            else this.isTeleported = false;
+        }
+        return true;
     }
 
     @Override
     public void addedToScene(Scene scene){
         super.addedToScene(scene);
         Player player = (Player) getScene().getFirstActorByName("Player");
-        new Loop<>(new Invoke<>(this::teleportPlayer)).scheduleFor(player);
+        new Loop<>(
+            new When<>(
+                () -> this.wasTeleported(player) == true,
+                new Invoke<>(this::teleportPlayer)
+            )
+        ).scheduleFor(player);
     }
 }
