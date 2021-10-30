85a088c7fe9ad9c5f0c19d2c380bf3a4abe2eaea
Kseniya <kseniya.shykina@student.tuke.sk>
2021-10-30 19:13:43 +0300

    teleporttt

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -28,18 +28,9 @@ public class Teleport extends AbstractActor {
     }
 
     public void teleportPlayer(Player player) {
-        if(this.itsTarget == null) return;
-        int x1, y1, x2, y2, x3, y3;
-        x1 = this.getPosX() + this.getWidth() / 2;
-        y1 = this.getPosY() + this.getHeight() / 2;
-        x2 = this.itsTarget.getPosX() + this.itsTarget.getWidth() / 6;
-        y2 = this.itsTarget.getPosY() + this.itsTarget.getHeight() / 6;
-        x3 = player.getPosX() + player.getWidth() / 2;
-        y3 = player.getPosY() + player.getHeight() / 2;
-        if(this.wasTeleported(player) == true && (x3 > x1 - this.getWidth()/2) && (y3 < y1 + this.getHeight()/2) && (x3 < x1 + this.getWidth()/2) && (y3 > y1 - this.getHeight()/2) ) {
-            player.setPosition(x2, y2);
-            this.itsTarget.isTeleported = true;
-        }
+        if(player == null) return;
+        player.setPosition(this.getPosX() + this.getWidth() / 6, this.getPosY() + this.getHeight() / 6);
+        this.isTeleported = true;
     }
 
     public boolean wasTeleported(Player player){
@@ -51,7 +42,12 @@ public class Teleport extends AbstractActor {
     }
 
     public void canTeleport(Player player){
-        if(this.wasTeleported(player) == true) this.teleportPlayer(player);
+        int x1, y1, x3, y3;
+        x1 = this.getPosX() + this.getWidth() / 2;
+        y1 = this.getPosY() + this.getHeight() / 2;
+        x3 = player.getPosX() + player.getWidth() / 2;
+        y3 = player.getPosY() + player.getHeight() / 2;
+        if(this.wasTeleported(player) == true && (x3 > x1 - this.getWidth()/2) && (y3 < y1 + this.getHeight()/2) && (x3 < x1 + this.getWidth()/2) && (y3 > y1 - this.getHeight()/2) && this.itsTarget != null) this.itsTarget.teleportPlayer(player);
     }
 
     @Override
